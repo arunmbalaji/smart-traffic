e65de04e7b4c2160847d0ea93e8edf19c7ee6f7f
santhosh <urukonda@amazon.com>
2023-10-25 22:32:49 +0530

    update config

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 MQTT_PORT = "8883"
 MQTT_HOST = "a123456789-ats.iot.ap-south-1.amazonaws.com"
 MQTT_QOS = 0
-THING_ID = "su_test_esp32"
+THING_ID = "car02"
 
-THING_CLIENT_CERT = "botid21-certificate.pem.crt"
-THING_PRIVATE_KEY = "botid21-private.pem.key"
+THING_CLIENT_CERT = "car02-certificate.pem.crt"
+THING_PRIVATE_KEY = "car02-private.pem.key"
 BOT_SPEED = 500
-BOT_ID = "21"
+BOT_ID = "car02"
--- ugit.py
+++ ugit.py
@@ -29,15 +29,15 @@ user = 'arunmbalaji'
 repository = 'smart-traffic'
 token = ''
 # Change this variable to 'master' or any other name matching your default branch
-default_branch = 'bot-id21'
+default_branch = 'car02'
 
 # Don't remove ugit.py from the ignore_files unless you know what you are doing :D
 # Put the files you don't want deleted or updated here use '/filename.ext'
 ignore_files = ['ugit.py', 
     'boot.py', 
     'config.py', 
-    'botid21-certificate.pem.crt', 
-    'botid21-private.pem.key',
+    'car02-certificate.pem.crt', 
+    'car02-private.pem.key',
     'umqtt.simple.py',
     'pipe.py',
     'worker.py'
