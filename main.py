2fa754177b6df7a5694363d23d1cd80da9c4eaaa
ArunBalaji <arunbal@amazon.com>
2023-10-25 13:40:16 +0530

    fixes to main.py

--- main.py
+++ main.py
@@ -3,6 +3,40 @@ from machine import Pin, PWM
 from time import sleep
 from TB6612FNG import Motor
 
+
+def test_motors():
+    print("starting the testing")
+    sleep(5)
+
+    print("moving forward")
+    motor.forward(500)
+    sleep(5)
+
+    print("moving backward")
+    motor.backward(500)
+    sleep(5)
+
+    print("moving right")
+    motor.right(500)
+    sleep(5)
+
+    print("moving left")
+    motor.left(500)
+    sleep(5)
+
+    motor.brake()
+    sleep(5)
+
+    motor.stop()
+    sleep(2)
+
+    motor.standby()
+    sleep(2)
+
+    motor.run()
+    sleep(2)
+
+
 frequency = 50
 
 PWMA = 15
@@ -19,70 +53,8 @@ ofsetB = 1
 
 motor = Motor(BIN2, BIN1, STBY, AIN1, AIN2, PWMA, PWMB, ofsetA, ofsetB)
 
-print("this is bot id 21")
+print("this is bot id 22")
 
-print("Second commit - not moving at all")
-# motor.right(800)
-# sleep(60)
-
-# print("only only only moving forward")
-# motor.forward(500)
-# sleep(10)
-
-# print("moving backward")
-# motor.backward(500)
-# sleep(5)
-
-
-# print("moving right")
-# motor.right(500)
-# sleep(20)
-
-
-# print("moving left")
-# motor.left(500)
-# sleep(20)
-
-
-# print("moving forward")
-# motor.forward(500)
-# sleep(10)
-
-# print("moving right")
-# motor.right(500)
-# sleep(4)
-
-# print("moving forward")
-# motor.forward(500)
-# sleep(10)
-
-# print("moving right")
-# motor.right(500)
-# sleep(4)
-
-# print("moving forward")
-# motor.forward(500)
-# sleep(10)
-
-# motor.backward(600)
-# sleep(10)
-
-# print("moving right")
-# motor.right(700)
-# sleep(10)
-
-# motor.left(800)
-# sleep(10)
-
-# motor.brake()
-# sleep(5)
-
-# motor.stop()
-# sleep(5)
-
-motor.standby()
-sleep(5)
+test_motors()
 
 print("Finished the execution. Coming out of the loop. Restart to start the loop again.")
-# motor.run()
-# sleep(5)
